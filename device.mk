275952803fdb4ad7592effead7c8a933702d57ed
Alexandre Rangel <mamutal91@gmail.com>
2023-03-08 19:13:18 -0300

    veux: Don't build Camera2 anymore
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- device.mk
+++ device.mk
@@ -76,7 +76,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Camera2 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
